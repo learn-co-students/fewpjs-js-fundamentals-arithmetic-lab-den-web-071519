6ae947d7e236804a2a9bbf5e290f46b6f68e5a69
lukemenard <lukemenard@gmail.com>
2019-08-29 17:09:41 -0600

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31
+let num2 = 2
+let num3 = 44
+let num4 = 10
 
-// Write your code here
+let multiply = num1*num2
+
+let random = Math.random()
+
+let mod = num3%num4
+
+let max = Math.max(20,19,18,17)
